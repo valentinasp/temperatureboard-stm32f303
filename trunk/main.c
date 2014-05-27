f606105e3f06b980d7258bda039eb00771ac0dc6
valentinasp@gmail.com <valentinasp@gmail.com@fb8316b1-0f57-896c-70e3-5f50f1a47a7f>
2014-05-27 14:41:20 +0000

    added CPAL

--- trunk/main.c
+++ trunk/main.c
@@ -52,6 +52,15 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+
+/* CPAL local transfer structures */
+CPAL_TransferTypeDef  sRxStructure, sTxStructure;
+uint8_t tRxBuffer[MAX_BUFF_SIZE];
+uint8_t tTxBuffer[256];
+uint32_t BufferSize = MAX_BUFF_SIZE;
+uint8_t Data[200];
+int8_t drv_i2c_err = 0;
+
 uint8_t KernelTask = TSK_IDLE;
 
 CanTxMsg TxMessage = {0};
@@ -65,6 +74,7 @@ uint16_t VirtAddVarTab[NB_OF_VAR] = {0x5555, 0x6666, 0x7777};
 uint16_t VarDataTab[NB_OF_VAR] = {0, 0, 0};
 
 RCC_ClocksTypeDef RCC_Clocks;
+__IO uint32_t ActionState = ACTION_NONE;
 
 /* Private function prototypes -----------------------------------------------*/
 static void CAN_Config(void);
@@ -73,7 +83,9 @@ static void CAN_Config(void);
 void SysTick_Configuration(void);
 void kernel(void);
 /* Private functions ---------------------------------------------------------*/
-
+static int8_t drv_i2c_EOT_Wait(void);
+static int8_t drv_i2c_WriteBuffer(const uint8_t *Data, uint16_t DataLength);
+static int8_t drv_i2c_ReadBuffer(uint8_t * buf, uint16_t len, uint16_t cmd);
 /**
   * @brief  Main program.
   * @param  None
@@ -148,6 +160,56 @@ int main(void)
    
   /* CAN configuration */
   CAN_Config();
+  
+  
+/* Start CPAL communication configuration ***********************************/
+
+  /* Initialize local Reception structures */
+  sRxStructure.wNumData = BufferSize;       /* Maximum Number of data to be received */
+  sRxStructure.pbBuffer = 0;//tRxBuffer;        /* Common Rx buffer for all received data */
+  sRxStructure.wAddr1 = 0x30;//0;                  /* Not needed */
+  sRxStructure.wAddr2 = 0;                  /* Not needed */
+  
+  /* Initialize local Transmission structures */
+  sTxStructure.wNumData = 0;//BufferSize;       /* Maximum Number of data to be received */
+  sTxStructure.pbBuffer = &tTxBuffer[0];     /* Common Rx buffer for all received data */
+  sTxStructure.wAddr1 = 0x30;//OWN_ADDRESS;        /* The own board address */
+  sTxStructure.wAddr2 = 0;//0;                  /* Not needed */
+  
+  /* Set HSI as I2C clock source */
+  RCC_I2CCLKConfig(RCC_I2C1CLK_HSI);
+  
+  /* Configure the device structure */
+  CPAL_I2C_StructInit(&I2C_DevStructure);      /* Set all fields to default values */
+  //I2C_DevStructure.CPAL_Mode = CPAL_MODE_SLAVE;
+  I2C_DevStructure.CPAL_Mode = CPAL_MODE_MASTER;
+  I2C_DevStructure.wCPAL_Options =  CPAL_OPT_NO_MEM_ADDR ;
+#ifdef CPAL_I2C_DMA_PROGMODEL
+  I2C_DevStructure.CPAL_ProgModel = CPAL_PROGMODEL_DMA;
+#elif defined (CPAL_I2C_IT_PROGMODEL)
+  I2C_DevStructure.CPAL_ProgModel = CPAL_PROGMODEL_INTERRUPT;
+#else
+ #error "Please select one of the programming model (in stm32f30x_i2c_cpal_conf.h)"
+#endif
+  I2C_DevStructure.pCPAL_I2C_Struct->I2C_Timing = I2C_TIMING;
+  I2C_DevStructure.pCPAL_I2C_Struct->I2C_OwnAddress1 = OWN_ADDRESS;
+  I2C_DevStructure.pCPAL_TransferRx = &sRxStructure;
+  I2C_DevStructure.pCPAL_TransferTx = &sTxStructure;
+  
+  /* Initialize CPAL device with the selected parameters */
+  CPAL_I2C_Init(&I2C_DevStructure);
+  
+//  delay(5000); /* Delay 5000 ms */
+  
+ //==========================
+  sRxStructure.wNumData = 0; /* Number of data to be read */
+  sRxStructure.pbBuffer = (uint8_t*)Data; /* Transmit buffer */
+  sRxStructure.wAddr1 = 0x30; /*Command register address*/
+  sRxStructure.wAddr2 = 0x00; /*Command register address*/
+  I2C1_DevStructure.pCPAL_TransferRx = &sRxStructure;
+  I2C1_DevStructure.wCPAL_Options &= ~CPAL_OPT_NO_MEM_ADDR;
+  I2C1_DevStructure.wCPAL_Options &= ~CPAL_OPT_16BIT_REG;//8 bit
+  
   
   /* Infinite loop */
   kernel();
@@ -244,6 +306,16 @@ void kernel(void)
       //TestProg();
       STM_EVAL_LEDToggle(LED1);
       STM_EVAL_LEDToggle(LED2);
+      
+      //Read redister
+      tTxBuffer[0] = 0x00;
+      drv_i2c_WriteBuffer(tTxBuffer,1); 
+      drv_i2c_ReadBuffer(tRxBuffer,1,0x00);
+
+      //Read redister
+      tTxBuffer[0] = 0x10;
+      drv_i2c_WriteBuffer(tTxBuffer,1); 
+      drv_i2c_ReadBuffer(tRxBuffer,4,0x10); 
     }
      /* =============================================================
     2,5 sec process
@@ -443,6 +515,88 @@ void assert_failed(uint8_t* file, uint32_t line)
 }
 
 #endif
+
+
+static int8_t drv_i2c_ReadBuffer(uint8_t * buf, uint16_t len, uint16_t cmd)
+{
+ /* If error return */
+ if(drv_i2c_err){return 1;}
+
+ /* Wait the end of transfer */
+ drv_i2c_err = drv_i2c_EOT_Wait();
+
+ if(!drv_i2c_err){
+   sRxStructure.wNumData = len;
+   sRxStructure.pbBuffer = buf;
+   sRxStructure.wAddr2 = cmd;
+   I2C_DevStructure.pCPAL_TransferRx = &sRxStructure;
+//   I2C_DevStructure.wCPAL_Options &= ~CPAL_OPT_NO_MEM_ADDR; /* send command right after address */
+   I2C_DevStructure.wCPAL_Options &= ~CPAL_OPT_NO_MEM_ADDR;
+   I2C_DevStructure.wCPAL_Options &= ~CPAL_OPT_16BIT_REG;
+   if(CPAL_I2C_Read(&I2C_DevStructure) != CPAL_PASS){
+     /* I2C bus or peripheral is not able to start communication: Error management */
+     drv_i2c_err = 1;
+   }else{
+     /* Wait the end of transfer */
+     drv_i2c_err = drv_i2c_EOT_Wait();
+   }
+ }
+ return drv_i2c_err;
+}
+
+static int8_t drv_i2c_WriteBuffer(const uint8_t *Data, uint16_t DataLength)
+{
+  /* If error return */
+  if(drv_i2c_err){return 1;}
+
+  
+  if(!drv_i2c_err){
+    I2C_DevStructure.wCPAL_Options |= CPAL_OPT_NO_MEM_ADDR;
+    I2C_DevStructure.wCPAL_Options &= ~CPAL_OPT_16BIT_REG;
+    
+    sTxStructure.wNumData = DataLength; /* Number of data to be written */
+    sTxStructure.pbBuffer = (uint8_t*)Data; /* Power Monitor buffer */
+    I2C_DevStructure.pCPAL_TransferTx = &sTxStructure;
+    //I2C1_DevStructure.wCPAL_Options |= CPAL_OPT_I2C_NOSTOP;
+    //I2C1_DevStructure.wCPAL_Options |= CPAL_OPT_I2C_NOSTOP_MODE;
+
+
+    /* Wait the end of transfer */
+    drv_i2c_err = drv_i2c_EOT_Wait();
+  
+    if(CPAL_I2C_Write(&I2C_DevStructure) != CPAL_PASS){
+      /* I2C bus or peripheral is not able to start communication: Error management */
+      drv_i2c_err = 1;
+    }
+    else{
+      /* Wait the end of transfer */
+      drv_i2c_err = drv_i2c_EOT_Wait();
+    }
+    
+  }
+
+  return drv_i2c_err;
+}
+
+/**
+  * @brief  Wait End-Of-Transfer of I2C driver .
+  * @param  None
+  * @retval : error
+  */
+static int8_t drv_i2c_EOT_Wait(void)
+{
+  /* If error return */
+  if(drv_i2c_err){return drv_i2c_err;}
+
+  /* Wait the end of transfer */
+  while(I2C_DevStructure.CPAL_State != CPAL_STATE_READY){
+    if(I2C_DevStructure.wCPAL_DevError != CPAL_I2C_ERR_NONE){
+      drv_i2c_err = 1;
+      break;
+    }
+  }
+  return drv_i2c_err;
+}
 
 /**
   * @}
