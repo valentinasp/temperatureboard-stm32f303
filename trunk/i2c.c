a1650204dfab5733821955dbef8c600b72bcfac4
valentinasp@gmail.com <valentinasp@gmail.com@fb8316b1-0f57-896c-70e3-5f50f1a47a7f>
2014-06-26 04:16:24 +0000

    moved I2C function to i2c.c file

---
+++ trunk/i2c.c
@@ -0,0 +1,163 @@
+/******************** (C) COPYRIGHT 2014 Valentinas Pavlovskis *****************
+* File Name          : i2c.c
+* Author             : vpa
+* Version            : V0.0.1
+* Date               : 25/06/2014
+* Description        : I2C functions
+********************************************************************************/
+
+/* Includes ------------------------------------------------------------------*/
+#include "i2c.h"
+#include <stdint.h>
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define MAX_BUFF_SIZE           200
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+/* CPAL local transfer structures */
+CPAL_TransferTypeDef  sRxStructure, sTxStructure;
+uint32_t BufferSize = MAX_BUFF_SIZE;
+uint8_t tRxBuffer[MAX_BUFF_SIZE];
+uint8_t tTxBuffer[MAX_BUFF_SIZE];
+int8_t drv_i2c_err = 0;
+/* Extern variables ----------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+static int8_t drv_i2c_EOT_Wait(void);
+static int8_t drv_i2c_WriteBuffer(const uint8_t *Data, uint16_t DataLength);
+static int8_t drv_i2c_ReadBuffer(uint8_t * buf, uint16_t len, uint16_t cmd);
+/* Private functions ---------------------------------------------------------*/
+
+void I2C_GetMagic(uint8_t* nr){
+  tTxBuffer[0] = 0x00;
+  drv_i2c_WriteBuffer(tTxBuffer,1); 
+  drv_i2c_ReadBuffer(tRxBuffer,1,0x00);
+  *nr = tRxBuffer[0];
+}
+
+static int8_t drv_i2c_ReadBuffer(uint8_t * buf, uint16_t len, uint16_t cmd)
+{
+ /* If error return */
+ if(drv_i2c_err){return 1;}
+
+ /* Wait the end of transfer */
+ drv_i2c_err = drv_i2c_EOT_Wait();
+
+ if(!drv_i2c_err){
+   sRxStructure.wNumData = len;
+   sRxStructure.pbBuffer = buf;
+   sRxStructure.wAddr2 = cmd;
+   I2C_DevStructure.pCPAL_TransferRx = &sRxStructure;
+   I2C_DevStructure.wCPAL_Options &= ~CPAL_OPT_NO_MEM_ADDR;
+   I2C_DevStructure.wCPAL_Options &= ~CPAL_OPT_16BIT_REG;
+   if(CPAL_I2C_Read(&I2C_DevStructure) != CPAL_PASS){
+     /* I2C bus or peripheral is not able to start communication: Error management */
+     drv_i2c_err = 1;
+   }else{
+     /* Wait the end of transfer */
+     drv_i2c_err = drv_i2c_EOT_Wait();
+   }
+ }
+ return drv_i2c_err;
+}
+
+static int8_t drv_i2c_WriteBuffer(const uint8_t *Data, uint16_t DataLength)
+{
+  /* If error return */
+  if(drv_i2c_err){return 1;}
+  
+  if(!drv_i2c_err){
+    I2C_DevStructure.wCPAL_Options |= CPAL_OPT_NO_MEM_ADDR;
+    I2C_DevStructure.wCPAL_Options &= ~CPAL_OPT_16BIT_REG;
+    
+    sTxStructure.wNumData = DataLength; /* Number of data to be written */
+    sTxStructure.pbBuffer = (uint8_t*)Data; /* Power Monitor buffer */
+    I2C_DevStructure.pCPAL_TransferTx = &sTxStructure;
+
+    /* Wait the end of transfer */
+    drv_i2c_err = drv_i2c_EOT_Wait();
+  
+    if(CPAL_I2C_Write(&I2C_DevStructure) != CPAL_PASS){
+      /* I2C bus or peripheral is not able to start communication: Error management */
+      drv_i2c_err = 1;
+    }
+    else{
+      /* Wait the end of transfer */
+      drv_i2c_err = drv_i2c_EOT_Wait();
+    }
+    
+  }
+
+  return drv_i2c_err;
+}
+
+/**
+  * @brief  Wait End-Of-Transfer of I2C driver .
+  * @param  None
+  * @retval : error
+  */
+static int8_t drv_i2c_EOT_Wait(void)
+{
+  /* If error return */
+  if(drv_i2c_err){return drv_i2c_err;}
+
+  /* Wait the end of transfer */
+  while(I2C_DevStructure.CPAL_State != CPAL_STATE_READY){
+    if(I2C_DevStructure.wCPAL_DevError != CPAL_I2C_ERR_NONE){
+      drv_i2c_err = 1;
+      break;
+    }
+  }
+  return drv_i2c_err;
+}
+
+void InitCPAL(void){
+  
+  /* Initialize local Reception structures */
+  sRxStructure.wNumData = BufferSize;       /* Maximum Number of data to be received */
+  sRxStructure.pbBuffer = 0;//tRxBuffer;        /* Common Rx buffer for all received data */
+  sRxStructure.wAddr1 = 0x30;//0;                  /* Not needed */
+  sRxStructure.wAddr2 = 0;                  /* Not needed */
+  
+  /* Initialize local Transmission structures */
+  sTxStructure.wNumData = 0;//BufferSize;       /* Maximum Number of data to be received */
+  sTxStructure.pbBuffer = &tTxBuffer[0];     /* Common Rx buffer for all received data */
+  sTxStructure.wAddr1 = 0x30;//OWN_ADDRESS;        /* The own board address */
+  sTxStructure.wAddr2 = 0;//0;                  /* Not needed */
+  
+  /* Set HSI as I2C clock source */
+  RCC_I2CCLKConfig(RCC_I2C1CLK_HSI);
+  
+  /* Configure the device structure */
+  CPAL_I2C_StructInit(&I2C_DevStructure);      /* Set all fields to default values */
+  I2C_DevStructure.CPAL_Mode = CPAL_MODE_MASTER;
+  I2C_DevStructure.wCPAL_Options =  CPAL_OPT_NO_MEM_ADDR ;
+#ifdef CPAL_I2C_DMA_PROGMODEL
+  I2C_DevStructure.CPAL_ProgModel = CPAL_PROGMODEL_DMA;
+#elif defined (CPAL_I2C_IT_PROGMODEL)
+  I2C_DevStructure.CPAL_ProgModel = CPAL_PROGMODEL_INTERRUPT;
+#else
+ #error "Please select one of the programming model (in stm32f30x_i2c_cpal_conf.h)"
+#endif
+  I2C_DevStructure.pCPAL_I2C_Struct->I2C_Timing = I2C_TIMING;
+  I2C_DevStructure.pCPAL_I2C_Struct->I2C_OwnAddress1 = OWN_ADDRESS;
+  I2C_DevStructure.pCPAL_TransferRx = &sRxStructure;
+  I2C_DevStructure.pCPAL_TransferTx = &sTxStructure;
+  
+  /* Initialize CPAL device with the selected parameters */
+  CPAL_I2C_Init(&I2C_DevStructure);
+  
+  sRxStructure.wNumData = 0; /* Number of data to be read */
+  sRxStructure.pbBuffer = &tRxBuffer[0];//(uint8_t*)Data; /* Transmit buffer */
+  sRxStructure.wAddr1 = 0x30; /*Command register address*/
+  sRxStructure.wAddr2 = 0x00; /*Command register address*/
+  I2C1_DevStructure.pCPAL_TransferRx = &sRxStructure;
+  I2C1_DevStructure.wCPAL_Options &= ~CPAL_OPT_NO_MEM_ADDR;
+  I2C1_DevStructure.wCPAL_Options &= ~CPAL_OPT_16BIT_REG;//8 bit
+  
+}
+
+void SetBoardAddress (uint32_t Addr){
+  sTxStructure.wAddr1=Addr;
+  sRxStructure.wAddr1=Addr;
+}
---
+++ trunk/i2c.h
@@ -0,0 +1,41 @@
+/******************** (C) COPYRIGHT 2014 Valentina Pavlovskis ******************
+* File Name          : i2c.h
+* Author             : vpa
+* Version            : V1.0.0
+* Date               : 25/06/2014
+* Description        : I2C functions
+*******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __I2C_H
+#define __I2C_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f30x.h"
+#include "stm32303c_eval.h"
+#include "stm32f30x_i2c_cpal.h"
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported define -----------------------------------------------------------*/
+#define OWN_ADDRESS             0x68
+#define I2C_TIMING              0x40110405 
+#define I2C_DevStructure        I2C1_DevStructure
+
+#define ACTION_NONE             0xFF
+#define ACTION_DISABLED         0xFD
+#define ACTION_PENDING          0xFE
+#define ACTION_PERIODIC         0xFC
+
+#define STATE_OFF               0
+#define STATE_ON                1
+/* Exported functions ------------------------------------------------------- */
+void SetBoardAddress (uint32_t Addr);
+void I2C_GetMagic(uint8_t* nr);
+/* External variables --------------------------------------------------------*/
+
+
+
+#endif  /*__I2C_H*/
+
+/******************* (C) COPYRIGHT 2011 Valentina Pavlovskis ****END OF FILE****/
